6cd46385595fd0307c4beb2f8bde0b44c852f6fa
Sergey Syrtsev <syrtsevser@gmail.com>
2024-10-16 07:00:33 +0300

    "Mute on minimize" uploaded.

---
+++ MuteOnMinimize/MuteAudioManager.cs
@@ -0,0 +1,55 @@
+﻿using BepInEx;
+using HarmonyLib;
+using JetBrains.Annotations;
+using System.Reflection;
+using UnityEngine;
+
+namespace MuteOnMinimize
+{
+	/// <summary>
+	/// Audio manager for muting game on minimize.
+	/// </summary>
+	[BepInPlugin(PluginId, "Mute On Minimize", "1.0.0")]
+	public class MuteAudioManager : BaseUnityPlugin
+	{
+		/// <summary>
+		/// Plugin identifier.
+		/// </summary>
+		private const string PluginId = "Mugnum.MuteOnMinimize";
+
+		/// <summary>
+		/// Harmony.
+		/// </summary>
+		private Harmony _harmony;
+
+		/// <summary>
+		/// Process intialization of the mod.
+		/// </summary>
+		[UsedImplicitly]
+		protected void Awake()
+		{
+			_harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), PluginId);
+			Application.focusChanged -= OnFocusChanged;
+			Application.focusChanged += OnFocusChanged;
+		}
+
+		/// <summary>
+		/// Process unloading of the mod.
+		/// </summary>
+		[UsedImplicitly]
+		protected void OnDestroy()
+		{
+			Application.focusChanged -= OnFocusChanged;
+			_harmony?.UnpatchSelf();
+		}
+
+		/// <summary>
+		/// Handle focus change.
+		/// </summary>
+		/// <param name="isFocused"> Is game in focus (not minimized). </param>
+		private static void OnFocusChanged(bool isFocused)
+		{
+			AudioListener.pause = !isFocused;
+		}
+	}
+}
